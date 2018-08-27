redhat-cip/dci-ansible-agent

Allow a partner to run their own tests
This commit creates a new hook `tests.yml` to allow partners to run
their own test suite.

Change-Id: I0c39f82ab0e91516b1a92f009cd49a861d563b1e

dci-ansible-agent.spec
@@ -47,6 +47,7 @@ install -p -D -m 644 hooks/pre-run.yml %{buildroot}%{_sysconfdir}/dci-ansible-ag
 install -p -D -m 644 hooks/running.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/running.yml
 install -p -D -m 644 hooks/teardown.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/teardown.yml
 install -p -D -m 644 hooks/success.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/success.yml
+install -p -D -m 644 hooks/tests.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/tests.yml
 install -p -D -m 644 settings.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/settings.yml
 install -p -D -m 644 dci-ansible-agent.sudo %{buildroot}%{_sysconfdir}/sudoers.d/dci-ansible-agent
 install -p -d -m 755 %{buildroot}/%{_sharedstatedir}/dci-ansible-agent
@@ -86,6 +87,7 @@ exit 0
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/pre-run.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/running.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/success.yml
+%config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/tests.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/teardown.yml
 %dir %{_sharedstatedir}/dci-ansible-agent
 %attr(0755, %{name}, %{name}) %{_sharedstatedir}/dci-ansible-agent

redhat-cip/dci-openstack-agent

dci-ansible-agent.spec
@@ -47,6 +47,7 @@ install -p -D -m 644 hooks/pre-run.yml %{buildroot}%{_sysconfdir}/dci-ansible-ag
 install -p -D -m 644 hooks/running.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/running.yml
 install -p -D -m 644 hooks/teardown.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/teardown.yml
 install -p -D -m 644 hooks/success.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/success.yml
+install -p -D -m 644 hooks/local_tests.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/hooks/local_tests.yml
 install -p -D -m 644 settings.yml %{buildroot}%{_sysconfdir}/dci-ansible-agent/settings.yml
 install -p -D -m 644 dci-ansible-agent.sudo %{buildroot}%{_sysconfdir}/sudoers.d/dci-ansible-agent
 install -p -d -m 755 %{buildroot}/%{_sharedstatedir}/dci-ansible-agent
@@ -86,6 +87,7 @@ exit 0
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/pre-run.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/running.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/success.yml
+%config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/local_tests.yml
 %config(noreplace) %{_sysconfdir}/dci-ansible-agent/hooks/teardown.yml
 %dir %{_sharedstatedir}/dci-ansible-agent
 %attr(0755, %{name}, %{name}) %{_sharedstatedir}/dci-ansible-agent